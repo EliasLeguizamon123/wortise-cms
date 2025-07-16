2af54c439f84c5e775b072dbd2c5fa7406a650ab
Elias <elias.leguizamon1997@gmail.com>
2025-07-16 17:59:30 -0300

    FEAT: complete trpc logic with register and login logic

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,15 +1,52 @@
-import { z } from 'zod';
+import { loginSchema, registerSchema } from '@/lib/zodSchemas.lib';
 import { baseProcedure, createTRPCRouter } from '../init';
+import { v4 as uuidv4 } from "uuid";
+import clientPromise from '@/lib/database.lib';
+import { TRPCError } from '@trpc/server';
+
 export const appRouter = createTRPCRouter({
-    hello: baseProcedure
-        .input(
-            z.object({
-                text: z.string(),
-            }),
-        )
-        .query((opts) => {
+    register: baseProcedure
+        .input(registerSchema)
+        .mutation(async ({ input }) => {
+            const client = await clientPromise;
+            const db = client.db();
+            const users = db.collection("users");
+
+            const existingUser = await users.findOne({ email: input.email });
+
+            if (existingUser) {
+                throw new TRPCError({
+                    code: "CONFLICT",
+                    message: "Usuario existente",
+                });
+            }
+
+            await users.insertOne(input);
+
             return {
-                greeting: `hello ${opts.input.text}`,
+                message: "Usuario registrado exitosamente",
+                token: uuidv4(),
+            };
+        }),
+    login: baseProcedure
+        .input(loginSchema)
+        .mutation(async ({ input }) => {
+            const client = await clientPromise;
+            const db = client.db();
+            const users = db.collection("users");
+
+            const user = await users.findOne({ email: input.email });
+
+            if (!user || user.password !== input.password) {
+                throw new TRPCError({
+                    code: "UNAUTHORIZED",
+                    message: "Credenciales incorrectas",
+                });
+            }
+
+            return {
+                message: "Login exitoso",
+                token: uuidv4(),
             };
         }),
 });
